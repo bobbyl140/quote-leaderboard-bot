49a9339f0ba85cd4560b1ac08270b40a5281edc3
bobbyl140 <bobbyl140@bobbyl140.com>
2023-11-28 01:31:54 -0500

    Make bot add reaction to quoted messages

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ bot = discord.Bot(intents=intents)
 
 # Global vars
 userid_regex = re.compile('<@[0-9]*>')
+emoji = '\N{THUMBS UP SIGN}'
 
 # Functions
 async def getid(idnum: int):
@@ -62,6 +63,7 @@ async def on_message(message):
 				if userid_regex.search(message.content):
 					print(f'{message.author.name} ({message.author.id}) quoted: \"{message.content}\"')
 					update_leaderboard(message.mentions)
+					await message.add_reaction(emoji)
 					await message.channel.send("**Quoted!** New totals:\n" + await get_formatted_leaderboard(), reference=message, silent=True, delete_after=15)
 				else:
 					to_delete_message = await message.channel.send("You didn't mention anyone. If this is a quote, please delete your message and send it again with the person's name mentioned. (This message will delete itself.)", reference=message, delete_after=15)
